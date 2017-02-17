2f71bfae7ffa6a4c49af55a89e9efc327bf8691d
StoyankaNikiforova <tanija@abv.bg>
2017-02-17 13:55:16 +0200

    empty

--- Course_System/courses/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Course(models.Model):
-    name = models.CharField(max_length=20, unique=True)
-    discription = models.TextField(max_length=100)
-    start_date = models.DateField(blank=True, null=True)
-    end_date = models.DateField(blank=True, null=True)
-    
